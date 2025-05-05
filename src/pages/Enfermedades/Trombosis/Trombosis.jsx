f006a8002b7a0af88a9a981a1702d5d2af4e35da
JonarcasDev <JonarcasDev@users.noreply.github.com>
2025-05-04 19:12:41 -0500

    sprint1 2.2.0

--- src/pages/Enfermedades/Trombosis/Trombosis.jsx
+++ src/pages/Enfermedades/Trombosis/Trombosis.jsx
@@ -2,8 +2,10 @@ import "./Trombosis.css";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
-import Heart1 from "../../models3d/Heart1";
+import Heart1 from "../../models3d/Heart1"; 
+import Floor from "../../models3d/Floor";
 import { useRef } from "react";
+import Lights from "../../../lights/lights";
 
 const Trombosis = () => {
     // Referencia para la sección a la que se desplazará
@@ -18,16 +20,19 @@ const Trombosis = () => {
         <div>
             <h1>Trombosis</h1>
             <Canvas
+                shadows
                 camera={{
                     position: [2, 0, 5],
-                    fov: 0.65,
+                    fov: 2,
                 }}
             >
+                <Lights />
                 <OrbitControls />
                 <ambientLight intensity={1.5} />
                 <directionalLight position={[5, 5, 10]} intensity={5} />
 
                 <Heart1 />
+                <Floor/>
             </Canvas>
             <button className="scroll-button" onClick={handleScroll}>
                 Leer más
