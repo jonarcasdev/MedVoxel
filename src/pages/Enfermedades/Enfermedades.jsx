b5ffbb90be6071aa7a696b2ff7c26c928eb9cb01
Juan Cardona <juanfernandocardonasarria@gmail.com>
2025-05-08 18:04:03 -0500

    secciones enfermedad

--- src/pages/Enfermedades/Enfermedades.jsx
+++ src/pages/Enfermedades/Enfermedades.jsx
@@ -11,8 +11,32 @@ const Enfermedades = () => {
         <h1 >Enfermedades</h1>
         <p>{userData?.displayName}</p>
         <Outlet /> 
+
+        <div className="container">
+                          
+        </div>
+        <div className="container2">
+                          
+        </div>
+        <div className="container3">
+                          
+        </div>
+        <div className="container4">
+                          
+        </div>
+        <div className="container5">
+                          
+        </div>
+        <div className="container6">
+                          
+        </div>
+        <div className="container7">
+                          
+        </div>
+        <div className="container8">
+                          
+        </div>
     </div>
     )
-    
 }
 export default Enfermedades;
